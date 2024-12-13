e98bac64ea90cff49467774a5fede21eec1c8ba8
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-12-13 06:51:01 +0200

    Revert "recapitulare"
    This reverts commit d30087c3f429e8efeb1af464e886ca73169e6d84.

--- Assets/Scripts/Items/Equipment.cs
+++ Assets/Scripts/Items/Equipment.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
 public class Equipment : Item
 {
-   public EquipmentSlot equipSlot; // slot to store equipment in 
+   public EquipmentSlot equipSlot;
    public SkinnedMeshRenderer meshRenderer;
    public EquipemntMeshRegion[] coveredMeshRegions;
 
-   public int armourModifier; // increase/decrease in armour
-   public int damageModifier; // increase/decrease in damage
+   public int armourModifier;
+   public int damageModifier;
 
    public override void Use()
    {
@@ -16,7 +16,7 @@ public class Equipment : Item
       // equip the item
       EquipmentManager.instance.Equip(this);
       // remove from inventory
-      RemoveFromInventory(); 
+      RemoveFromInventory();
 
    }
 }
@@ -29,5 +29,5 @@ public enum EquipmentSlot
 // corresponds to body blendshapes
 public enum EquipemntMeshRegion 
 {
-   Legs, Arms, Torso // corresponds to body blend shapes
+   Legs, Arms, Torso 
 } 
